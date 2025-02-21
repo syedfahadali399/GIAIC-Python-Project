8f06b532e3df9a7c341f8f37b958617141488b41
Syed Fahad Ali <155745867+syedfahadali399@users.noreply.github.com>
2025-02-21 20:59:07 +0500

    Add files via upload

--- required_project/rock_paper_ scissor.py
+++ required_project/rock_paper_ scissor.py
@@ -6,26 +6,29 @@ def game(r):
 
     for r in range(0, r):
 
-        user_input = str(input("""Enter any word from => \n(r, p, s) to play game: """))
-        game_choices = ["r", "p", "s"]
-        random_chioce = choice(game_choices)
-    
-        if(user_input == random_chioce):
-            print("Its Draw")
-        elif(user_input == "r" and random_chioce == "p"):
-            print("You Lose")
-        elif(user_input == "r" and random_chioce == "s"):
-            print("You Win")
-        elif(user_input == "p" and random_chioce == "r"):
-            print("You Win")
-        elif(user_input == "p" and random_chioce == "s"):
-            print("You Lose")
-        elif(user_input == "s" and random_chioce == "r"):
-            print("You Lose")
-        elif(user_input == "s" and random_chioce == "p"):
-            print("You Win")
-        else:
+        user_input = str(input("What is your choice : r for rock, p for paper , s for scissor: "))
+        comp_choice =  choice(["r", "p", "s"])
+
+        if user_input not in ["r", "p", "s"]:
             print("Please Enter (r, p, s) to play game: ")
+            break
+        else: 
+            if(user_input == comp_choice):
+                print("Its Draw")
+            elif(user_input == "r" and comp_choice == "p"):
+                print("You Lose")
+            elif(user_input == "r" and comp_choice == "s"):
+                print("You Win")
+            elif(user_input == "p" and comp_choice == "r"):
+                print("You Win")
+            elif(user_input == "p" and comp_choice == "s"):
+                print("You Lose")
+            elif(user_input == "s" and comp_choice == "r"):
+                print("You Lose")
+            elif(user_input == "s" and comp_choice == "p"):
+                print("You Win")
+            else:
+                print("System Error run again")
     r += 1
 
 game(repeat_program)
