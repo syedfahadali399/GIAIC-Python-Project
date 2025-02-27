30c4f938c63a1fbd45000474160059ba198252e4
Syed Fahad Ali <155745867+syedfahadali399@users.noreply.github.com>
2025-02-27 20:58:08 +0500

    Delete homework-project/00-intro-python directory

--- homework-project/00-intro-python/00-intro-python/01_add_two_number.py
+++
@@ -1,9 +0,0 @@
-def sum():
-    first_number = int(input("Enter First Number: "))
-    second_number = int(input("Enter Second Number: "))
-
-    result = first_number + second_number
-    return result
-
-show_result = sum()
-print(f"The Sum Of 2 Number is {show_result}")
--- homework-project/00-intro-python/00-intro-python/06_square_number.py
+++
@@ -1,9 +0,0 @@
-ask_num = int(input("Enter a Number to double the value: "))
-
-def square_number(value):
-
-    result = value * 2
-    return result
-
-show_result = square_number(ask_num)
-print(f"The Square value of {ask_num} is {show_result}")
