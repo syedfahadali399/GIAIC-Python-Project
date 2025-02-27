bc66707cdc5ba03cd761f69a0da3c02e1ad87eec
Syed Fahad Ali <155745867+syedfahadali399@users.noreply.github.com>
2025-02-27 20:57:11 +0500

    Delete 00-intro-python directory

--- 00-intro-python/02_agremment_bot.py
+++
@@ -1,6 +0,0 @@
-def bot():
-    input_user = str(input("What is your Favoriate Animal: "))
-    return input_user
-
-show_user = bot()
-print(f"Wow! My Favoraite Animal is also {show_user}")
--- 00-intro-python/03_fahrenheit_to_celsius.py
+++
@@ -1,8 +0,0 @@
-def fahren_cel():
-    input_user = float(input("Enter Fahrenheit Temprature in your area: "))
-
-    cal = (input_user - 32) * 5 / 9
-    return cal
-
-show_value = fahren_cel()
-print(f"The Temprature in Celsius is {round(show_value, 2)}°C")
