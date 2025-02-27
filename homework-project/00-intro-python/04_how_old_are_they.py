50cc5a73563a5d6fd3bdafdfc34eab47da5b3118
Syed Fahad Ali <155745867+syedfahadali399@users.noreply.github.com>
2025-02-27 20:22:47 +0500

    Add files via upload

---
+++ homework-project/00-intro-python/04_how_old_are_they.py
@@ -0,0 +1,11 @@
+anton_age = 21
+beth_age = anton_age + 6
+chen_age = beth_age + 20
+drew_age = chen_age + anton_age
+ethan_age = chen_age
+
+print(f"The Anton Age is {anton_age}")
+print(f"The Beth Age is {beth_age}")
+print(f"The Chen Age is {chen_age}")
+print(f"The Drew Age is {drew_age}")
+print(f"The Ethan Age is {ethan_age}")
