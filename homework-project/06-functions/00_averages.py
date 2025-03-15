47b1b6b2cc0f9a38760464650a2a473b07efc425
Syed Fahad Ali <155745867+syedfahadali399@users.noreply.github.com>
2025-03-15 19:52:13 +0500

    Add files via upload

---
+++ homework-project/06-functions/00_averages.py
@@ -0,0 +1,9 @@
+def average_num():
+    num_1 = int(input("Enter Number 1 to find average num: "))
+    num_2 = int(input("Enter Number 2 to find average num: "))
+
+    average = (num_1 + num_2) / 2
+    return average
+
+result = average_num()
+print(result)
