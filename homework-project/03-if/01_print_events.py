9f81643d8c434ca415e74e41e32b19099d598c37
Syed Fahad Ali <155745867+syedfahadali399@users.noreply.github.com>
2025-03-21 01:46:04 +0500

    Delete homework-project/03-if directory

--- homework-project/03-if/01_print_events.py
+++
@@ -1,5 +0,0 @@
-def even_number():
-    for i in range(20):
-        print(i * 2)
-
-even_number()
--- homework-project/03-if/05_random_numbers.py
+++
@@ -1,8 +0,0 @@
-import random
-
-def main():
-    for i in range(10):
-        i = random.randint(1, 100)
-        print(i)
-
-main()
