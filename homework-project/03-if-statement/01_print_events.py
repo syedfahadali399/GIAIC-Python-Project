f91b2cb1e20d4b64616444c1dde5a6addf40269f
Syed Fahad Ali <155745867+syedfahadali399@users.noreply.github.com>
2025-03-21 01:46:29 +0500

    Add files via upload

---
+++ homework-project/03-if-statement/01_print_events.py
@@ -0,0 +1,5 @@
+def even_number():
+    for i in range(20):
+        print(i * 2)
+
+even_number()
---
+++ homework-project/03-if-statement/03_leap_year.py
@@ -0,0 +1,16 @@
+def main():
+    year = int(input('Please input a year: '))
+
+    if year % 4 == 0: 
+        if year % 100 == 0: 
+            if year % 400 == 0: 
+                print("That's a leap year!")
+                
+            else: 
+                print("That's not a leap year.")
+        else:  
+            print("That's a leap year!")
+    else: 
+        print("That's not a leap year.")
+        
+main()
